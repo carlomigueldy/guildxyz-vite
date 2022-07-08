7e3a95d5a2b563c1ed7ec27c97e19023290240eb
Carlo Miguel Dy <carlomigueldy@gmail.com>
2022-07-08 17:16:01 +0800

    Add connect wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
+import { useConnect } from "wagmi";
 import { useCreateGuild } from "./hooks";
 
 function App() {
+  const {
+    data: connectData,
+    connectAsync,
+    connectors,
+    isLoading: connectLoading,
+  } = useConnect();
+  const [metamask] = connectors;
   const {
     data: createGuildData,
     isLoading: createGuildLoading,
     mutateAsync: createGuild,
   } = useCreateGuild({ name: "AW Test Guild", roles: [] });
+  const isConnected = !!connectData;
 
   return (
     <>
+      <h1>{isConnected ? "web3 enabled" : "not connected"}</h1>
+
+      <button onClick={() => connectAsync({ connector: metamask })}>
+        {connectLoading ? "connecting wallet..." : "Connect Wallet"}
+      </button>
+
       <button onClick={() => createGuild()}>
         {createGuildLoading ? "..." : "Create Guild"}
       </button>
--- src/main.tsx
+++ src/main.tsx
@@ -19,7 +19,7 @@ const { chains, provider, webSocketProvider } = configureChains(
 );
 
 const client = createClient({
-  autoConnect: true,
+  autoConnect: false,
   provider,
   webSocketProvider,
 });
